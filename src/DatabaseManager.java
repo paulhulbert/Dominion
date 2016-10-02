e41edf835a958786b951ecce5ca7eb9695ce382b
Paul Hulbert <pxh8242@rit.edu>
2016-10-01 18:47:14 -0700

    Connected to database

---
+++ src/DatabaseManager.java
@@ -0,0 +1,26 @@
+import java.sql.*;
+
+/**
+ * Created by paulh on 10/1/2016.
+ */
+public class DatabaseManager {
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://204.246.56.27/dominion", "dominion", "La9mT6?92K!G");
+
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM actions");
+
+
+            while (rs.next()) {
+                System.out.println(rs.getString(4));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
